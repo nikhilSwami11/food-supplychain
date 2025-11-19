f4abd384023a5ee715113d75834c09f1ff995796
nikhilSwami11 <nikhilswamidevs@gmail.com>
2025-11-19 09:02:42 -0700

    added features in smart contract

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -2,13 +2,14 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract SupplyChain {
+    // 1. Update State Enum to include 'Ordered'
     enum State {
         Created,
+        Ordered,
         InTransit,
         Stored,
         Delivered
     }
-
     struct Product {
         uint256 id;
         string name;
@@ -18,7 +19,11 @@ contract SupplyChain {
         address[] ownershipHistory;
         State state;
         string ipfsHash;
+        address orderedBy;
     }
+
+    // Array to keep track of all IDs (Required for "View" functions)
+    uint256[] public allProductIds;
 
     mapping(uint256 => Product) public products;
 
@@ -94,10 +99,94 @@ contract SupplyChain {
             isAuthentic: true,
             ownershipHistory: initialHistory,
             state: State.Created,
-            ipfsHash: _ipfsHash
+            ipfsHash: _ipfsHash,
+            orderedBy: address(0)
         });
 
+        //Save ID to array so we can list it later
+        allProductIds.push(_id);
+
         emit ProductRegistered(_id, _name, msg.sender);
+    }
+
+    function getAvailableProducts() public view returns (Product[] memory) {
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < allProductIds.length; i++) {
+            if (products[allProductIds[i]].state == State.Created) {
+                count++;
+            }
+        }
+
+        Product[] memory availableItems = new Product[](count);
+        uint256 index = 0;
+
+        for (uint256 i = 0; i < allProductIds.length; i++) {
+            if (products[allProductIds[i]].state == State.Created) {
+                availableItems[index] = products[allProductIds[i]];
+                index++;
+            }
+        }
+
+        return availableItems;
+    }
+
+    function placeOrder(uint256 _id) public {
+        require(products[_id].id != 0, "SC: Product does not exist");
+        require(
+            products[_id].state == State.Created,
+            "SC: Product not available for order"
+        );
+
+        products[_id].state = State.Ordered;
+        products[_id].orderedBy = msg.sender;
+    }
+
+    function getFarmerRequests() public view returns (Product[] memory) {
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < allProductIds.length; i++) {
+            Product memory p = products[allProductIds[i]];
+            // Condition: I am the owner AND it has been ordered
+            if (p.currentOwner == msg.sender && p.state == State.Ordered) {
+                count++;
+            }
+        }
+
+        Product[] memory requests = new Product[](count);
+        uint256 index = 0;
+
+        for (uint256 i = 0; i < allProductIds.length; i++) {
+            Product memory p = products[allProductIds[i]];
+            if (p.currentOwner == msg.sender && p.state == State.Ordered) {
+                requests[index] = p;
+                index++;
+            }
+        }
+
+        return requests;
+    }
+
+    function getMyOrders() public view returns (Product[] memory) {
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < allProductIds.length; i++) {
+            if (products[allProductIds[i]].orderedBy == msg.sender) {
+                count++;
+            }
+        }
+
+        Product[] memory myOrders = new Product[](count);
+        uint256 index = 0;
+
+        for (uint256 i = 0; i < allProductIds.length; i++) {
+            if (products[allProductIds[i]].orderedBy == msg.sender) {
+                myOrders[index] = products[allProductIds[i]];
+                index++;
+            }
+        }
+
+        return myOrders;
     }
 
     function transferOwnership(uint256 _id, address _newOwner) public {
